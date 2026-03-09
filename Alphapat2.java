b5bbbf7f6b9613256773d706903a77be51429c67
ashmitha400-git <ashmitha400@gmail.com>
2026-03-09 20:18:22 +0530

    Add files via upload

---
+++ Alphapat2.java
@@ -0,0 +1,13 @@
+class Alphapat2{
+public static void main(String args[]){
+for(int r=65;r<68;r++){
+for(int c=65;c<68;c++){
+if(r==66 && c==66)
+System.out.print(" ");
+else
+System.out.print((char)c);
+}
+System.out.println();
+}
+}
+}
