e1ee5e5d2a7c40625ace19c7774d1147c73f57bb
ashmitha400-git <ashmitha400@gmail.com>
2026-03-19 13:19:12 +0530

    Add files via upload

---
+++ Withargs.java
@@ -0,0 +1,14 @@
+package javafullstack;
+
+public class Withargs {
+	public static void main(String[] args) {
+		Withargs ag=new Withargs();
+		ag.addition(30,40);
+	}
+	void addition(int a,int b) {
+		
+		int add=a+b;
+		System.out.println("add:"+add);
+	}
+
+}
